40f02fb21bd9d6ca2a6a96f314269d86bd45981d
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-26 18:16:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ function currentLine(katzDeliLine) {
   var line = [];
   
   if katzDeliLine.length === 0 {
+    return 
     for (var i = 0; i < katzDeliLine.length; i++) {
-     line.push(` ${i + 1}. ${katzDeliLine[i]}`)
+    line.push(` ${i + 1}. ${katzDeliLine[i]}`)
   }
   return('The line is currently:' + line)
 }
