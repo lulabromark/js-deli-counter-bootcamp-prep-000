f6a04af07d6821808237bd4f07d9f26a115d9fdd
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-26 17:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   var line = [];
+  line.push(`1. ${katzDeliLine[0]}`)
   for (var i = 0; i < katzDeliLine.length; i++) {
      line.push(` ${i + 1}. ${katzDeliLine[i]}`)
   }
